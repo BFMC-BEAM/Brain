3ec20c6e904b5462cdaf61fb54e4a9a6ad0b9829
miroagustin <miroagustin11@gmail.com>
2025-02-22 01:02:34 -0300

    added constants to state machine

--- src/decision/decisionMaker/stateMachine.py
+++ src/decision/decisionMaker/stateMachine.py
@@ -1,12 +1,9 @@
 import time
 from src.decision.distance.distanceModule import DistanceModule
 from src.decision.lineFollowing.purepursuit import ControlSystem
-from src.utils.constants import BRAINLESS, CONTROL_FOR_SIGNS
-from src.utils.messages import messageHandlerSender
+from src.utils.constants import START_STATE, LANE_FOLLOWING, CLASSIFYING_SIGN, STOP_CAR, PARKING_STATE, WAITING_FOR_PEDESTRIAN, OVERTAKING_MOVING_CAR, OVERTAKING_STATIC_CAR, AVOIDING_ROADBLOCK, CLASSIFYING_OBSTACLE, TAILING_CAR, APPROACHING_STOP_LINE, CROSSWALK_NAVIGATION, ROUNDABOUT_NAVIGATION, TRACKING_LOCAL_PATH, INTERSECTION_NAVIGATION, WAITING_AT_STOPLINE, WAITING_FOR_GREEN, END_STATE, ROADMAP_LOADED, OBSTACLE_DISTANCE_THRESHOLD, SIGN_DISTANCE_THRESHOLD, END_EVENT, STOP_LINE_APPROACH_DISTANCE_THRESHOLD, PARKING_EVENT, CONTINUE_LANE_FOLLOWING, STOP_SIGN_DETECTED, TIMEOUT_STOP, PARKING_COMPLETED, PEDESTRIAN_TIMEOUT, CAR_OVERTAKEN, ROADBLOCK_AVOIDED, OBSTACLE_PEDESTRIAN, OBSTACLE_CAR, OBSTACLE_ROADBLOCK, IF_OBSTACLE_TOO_FAR, IF_MOVING, IF_STATIC, CROSSWALK_EVENT, TIMEOUT_CROSSWALK, INTERSECTION_TRAFFIC_LIGHT_EVENT, INTERSECTION_STOP_EVENT, JUNCTION_EVENT, INTERSECTION_PRIORITY_EVENT, ROUNDABOUT_EVENT, ALWAYS, END_OF_LOCAL_PATH, TIMEOUT_STOPLINE, SEMAPHORE_GREEN, TRY_PARKING, STOP, LOST
 
-ALWAYS_ON_ROUTINES = [CONTROL_FOR_SIGNS]
-LANE_FRAME_SKIP = 1
-SIGNAL_FRAME_SKIP = 25
+
 
 
 class Routine():
@@ -14,8 +11,10 @@ class Routine():
         self.name = name
         self.method = method
         self.active = activated
+
     def __str__(self):
         return self.name
+
     def run(self):
         self.method()
 
@@ -24,86 +23,81 @@ class StateMachine():
     def __init__(self):
         self.distance_module = DistanceModule()
         self.control_system = ControlSystem()
-        # INITIALIZE ROUTINES
-        # self.routines = {        
-        #    CONTROL_FOR_SIGNS: Routine(CONTROL_FOR_SIGNS,  self.control_for_signs),         
-        # }
-        # mat state
+        
         self.state_transitions = {
-            'start_state': {'ROADMAP_LOADED': 'lane_following'},
-            'lane_following': {
-                'OBSTACLE_DISTANCE_THRESHOLD': 'classifying_obstacle',
-                'SIGN_DISTANCE_THRESHOLD': 'classifying_sign',
-                'END_EVENT': 'end_state',
-                'STOP_LINE_APPROACH_DISTANCE_THRESHOLD': 'approaching_stop_line',
-                'PARKING_EVENT': 'parking_state',
-                'CONTINUE_LANE_FOLLOWING': 'lane_following',
+            START_STATE: {ROADMAP_LOADED: LANE_FOLLOWING},
+            LANE_FOLLOWING: {
+                OBSTACLE_DISTANCE_THRESHOLD: CLASSIFYING_OBSTACLE,
+                SIGN_DISTANCE_THRESHOLD: CLASSIFYING_SIGN,
+                END_EVENT: END_STATE,
+                STOP_LINE_APPROACH_DISTANCE_THRESHOLD: APPROACHING_STOP_LINE,
+                PARKING_EVENT: PARKING_STATE,
+                CONTINUE_LANE_FOLLOWING: LANE_FOLLOWING,
             },
-            'classifying_sign': {'STOP_SIGN_DETECTED': 'stop_car'},
-            'stop_car': {'TIMEOUT_STOP': 'lane_following'},
-            'parking_state': {'PARKING_COMPLETED': 'lane_following',
-                              'TRY_PARKING': 'parking_state'},
-            'waiting_for_pedestrian': {'PEDESTRIAN_TIMEOUT': 'lane_following'},
-            'overtaking_moving_car': {'CAR_OVERTAKEN': 'lane_following'},
-            'overtaking_static_car': {'CAR_OVERTAKEN': 'lane_following'},
-            'avoiding_roadblock': {'ROADBLOCK_AVOIDED': 'lane_following'},
-            'classifying_obstacle': {
-                'OBSTACLE=PEDESTRIAN': 'waiting_for_pedestrian',
-                'OBSTACLE=CAR': 'tailing_car',
-                'OBSTACLE=ROADBLOCK': 'avoiding_roadblock',
+            CLASSIFYING_SIGN: {STOP_SIGN_DETECTED: STOP_CAR},
+            STOP_CAR: {TIMEOUT_STOP: LANE_FOLLOWING},
+            PARKING_STATE: {PARKING_COMPLETED: LANE_FOLLOWING, PARKING_EVENT: PARKING_STATE},
+            WAITING_FOR_PEDESTRIAN: {PEDESTRIAN_TIMEOUT: LANE_FOLLOWING},
+            OVERTAKING_MOVING_CAR: {CAR_OVERTAKEN: LANE_FOLLOWING},
+            OVERTAKING_STATIC_CAR: {CAR_OVERTAKEN: LANE_FOLLOWING},
+            AVOIDING_ROADBLOCK: {ROADBLOCK_AVOIDED: LANE_FOLLOWING},
+            CLASSIFYING_OBSTACLE: {
+                OBSTACLE_PEDESTRIAN: WAITING_FOR_PEDESTRIAN,
+                OBSTACLE_CAR: TAILING_CAR,
+                OBSTACLE_ROADBLOCK: AVOIDING_ROADBLOCK,
             },
-            'tailing_car': {
-                'IF_OBSTACLE_TOO_FAR': 'lane_following',
-                'IF_MOVING': 'overtaking_moving_car',
-                'IF_STATIC': 'overtaking_static_car',
+            TAILING_CAR: {
+                IF_OBSTACLE_TOO_FAR: LANE_FOLLOWING,
+                IF_MOVING: OVERTAKING_MOVING_CAR,
+                IF_STATIC: OVERTAKING_STATIC_CAR,
             },
-            'approaching_stop_line': {
-                'CROSSWALK_EVENT': 'crosswalk_navigation',
-                'INTERSECTION_TRAFFIC_LIGHT_EVENT': 'waiting_for_green',
-                'INTERSECTION_STOP_EVENT': 'waiting_at_stopline',
-                'JUNCTION_EVENT': 'intersection_navigation',
-                'INTERSECTION_PRIORITY_EVENT': 'intersection_navigation',
-                'ROUNDABOUT_EVENT': 'roundabout_navigation',
+            APPROACHING_STOP_LINE: {
+                CROSSWALK_EVENT: CROSSWALK_NAVIGATION,
+                INTERSECTION_TRAFFIC_LIGHT_EVENT: WAITING_FOR_GREEN,
+                INTERSECTION_STOP_EVENT: WAITING_AT_STOPLINE,
+                JUNCTION_EVENT: INTERSECTION_NAVIGATION,
+                INTERSECTION_PRIORITY_EVENT: INTERSECTION_NAVIGATION,
+                ROUNDABOUT_EVENT: ROUNDABOUT_NAVIGATION,
             },
-            'crosswalk_navigation': {'TIMEOUT_CROSSWALK': 'lane_following'},
-            'roundabout_navigation': {'ALWAYS': 'tracking_local_path'},
-            'tracking_local_path': {'END_OF_LOCAL_PATH': 'lane_following'},
-            'intersection_navigation': {'ALWAYS': 'tracking_local_path'},
-            'waiting_at_stopline': {'TIMEOUT_STOPLINE': 'intersection_navigation'},
-            'waiting_for_green': {'SEMAPHORE_GREEN': 'intersection_navigation'}
+            CROSSWALK_NAVIGATION: {TIMEOUT_CROSSWALK: LANE_FOLLOWING},
+            ROUNDABOUT_NAVIGATION: {ALWAYS: TRACKING_LOCAL_PATH},
+            TRACKING_LOCAL_PATH: {END_OF_LOCAL_PATH: LANE_FOLLOWING},
+            INTERSECTION_NAVIGATION: {ALWAYS: TRACKING_LOCAL_PATH},
+            WAITING_AT_STOPLINE: {TIMEOUT_STOPLINE: INTERSECTION_NAVIGATION},
+            WAITING_FOR_GREEN: {SEMAPHORE_GREEN: INTERSECTION_NAVIGATION},
         }
         
         self.event_methods = {
-            'ROADMAP_LOADED': self.on_roadmap_loaded,
-            'OBSTACLE_DISTANCE_THRESHOLD': self.on_obstacle_detected,
-            'END_EVENT': self.on_end,
-            'STOP_LINE_APPROACH_DISTANCE_THRESHOLD': self.on_stop_line_approach,
-            'PARKING_EVENT': self.on_parking, 
-            'TRY_PARKING' : self.try_parking,
-            'CONTINUE_LANE_FOLLOWING': self.on_lane_following,
-            'STOP_SIGN_DETECTED': self.on_stop_sign_detected,
-            'TIMEOUT_STOP': self.on_timeout_stop,
-            'PARKING_COMPLETED': self.on_parking_completed,
-            'PEDESTRIAN_TIMEOUT': self.on_pedestrian_timeout,
-            'CAR_OVERTAKEN': self.on_car_overtaken,
-            'ROADBLOCK_AVOIDED': self.on_roadblock_avoided,
-            'OBSTACLE=PEDESTRIAN': self.on_pedestrian_detected,
-            'OBSTACLE=CAR': self.on_car_detected,
-            'OBSTACLE=ROADBLOCK': self.on_roadblock_detected,
-            'IF_OBSTACLE_TOO_FAR': self.on_obstacle_too_far,
-            'IF_MOVING': self.on_car_moving,
-            'IF_STATIC': self.on_car_static,
-            'CROSSWALK_EVENT': self.on_crosswalk_event,
-            'TIMEOUT_CROSSWALK': self.on_timeout_crosswalk,
-            'INTERSECTION_TRAFFIC_LIGHT_EVENT': self.on_waiting_for_green,
-            'INTERSECTION_STOP_EVENT': self.on_waiting_at_stopline,
-            'JUNCTION_EVENT': self.on_intersection_navigation,
-            'INTERSECTION_PRIORITY_EVENT': self.on_intersection_navigation,
-            'ROUNDABOUT_EVENT': self.on_roundabout_navigation,
-            'ALWAYS': self.on_always,
-            'END_OF_LOCAL_PATH': self.on_end_of_local_path,
-            'TIMEOUT_STOPLINE': self.on_timeout_stopline,
-            'SEMAPHORE_GREEN': self.on_semaphore_green,
+            ROADMAP_LOADED: self.on_roadmap_loaded,
+            OBSTACLE_DISTANCE_THRESHOLD: self.on_obstacle_detected,
+            END_EVENT: self.on_end,
+            STOP_LINE_APPROACH_DISTANCE_THRESHOLD: self.on_stop_line_approach,
+            PARKING_EVENT: self.on_parking, 
+            TRY_PARKING: self.try_parking,
+            CONTINUE_LANE_FOLLOWING: self.on_lane_following,
+            STOP_SIGN_DETECTED: self.on_stop_sign_detected,
+            TIMEOUT_STOP: self.on_timeout_stop,
+            PARKING_COMPLETED: self.on_parking_completed,
+            PEDESTRIAN_TIMEOUT: self.on_pedestrian_timeout,
+            CAR_OVERTAKEN: self.on_car_overtaken,
+            ROADBLOCK_AVOIDED: self.on_roadblock_avoided,
+            OBSTACLE_PEDESTRIAN: self.on_pedestrian_detected,
+            OBSTACLE_CAR: self.on_car_detected,
+            OBSTACLE_ROADBLOCK: self.on_roadblock_detected,
+            IF_OBSTACLE_TOO_FAR: self.on_obstacle_too_far,
+            IF_MOVING: self.on_car_moving,
+            IF_STATIC: self.on_car_static,
+            CROSSWALK_EVENT: self.on_crosswalk_event,
+            TIMEOUT_CROSSWALK: self.on_timeout_crosswalk,
+            INTERSECTION_TRAFFIC_LIGHT_EVENT: self.on_waiting_for_green,
+            INTERSECTION_STOP_EVENT: self.on_waiting_at_stopline,
+            JUNCTION_EVENT: self.on_intersection_navigation,
+            INTERSECTION_PRIORITY_EVENT: self.on_intersection_navigation,
+            ROUNDABOUT_EVENT: self.on_roundabout_navigation,
+            ALWAYS: self.on_always,
+            END_OF_LOCAL_PATH: self.on_end_of_local_path,
+            TIMEOUT_STOPLINE: self.on_timeout_stopline,
+            SEMAPHORE_GREEN: self.on_semaphore_green,
         }
         
         # PARKING
@@ -112,7 +106,7 @@ class StateMachine():
         self.parking_duration = [2, 1, 1, 1, 1]  
         self.parking_end_time = None  
         
-        self.current_state = 'parking_state' #start_state
+        self.current_state = PARKING_STATE
         self.current_speed = None
         self.current_steer = None
         self.current_direction = None
@@ -129,8 +123,6 @@ class StateMachine():
         else:
             print(f"No se puede cambiar al estado con el evento: {event} desde {self.current_state}")
     #===================== EVENT HANDLING =====================#
-        
-
     def handle_events(self, act_deviation, num_lines_detected, objects_detected, current_speed, current_steer, direction, ultra_values):
         self.current_deviation = act_deviation if act_deviation is not None else self.current_deviation
         self.num_lines_detected = num_lines_detected if num_lines_detected is not None else self.num_lines_detected
@@ -139,34 +131,34 @@ class StateMachine():
         self.current_steer = current_steer if current_steer is not None else self.current_steer
         self.current_direction = direction if direction is not None else self.current_direction
         self.current_ultra_values = ultra_values if ultra_values is not None else self.current_ultra_values
-        if self.current_state == 'lane_following':
+        if self.current_state == LANE_FOLLOWING:
             
             #if intersection == 1:
-            #    self.change_state('STOP_SIGN_DETECTED')
+            #    self.change_state(STOP_SIGN_DETECTED)
             #else:
-            self.change_state("CONTINUE_LANE_FOLLOWING")
-        elif self.current_state == 'classifying_sign':
+            self.change_state(CONTINUE_LANE_FOLLOWING)
+        elif self.current_state == CLASSIFYING_SIGN:
             for sign_name, valid_distance in objects_detected:
-                if sign_name == "STOP" and valid_distance:
-                    self.change_state("STOP_SIGN_DETECTED")
-        elif self.current_state == 'stop_car':
+                if sign_name == STOP and valid_distance:
+                    self.change_state(STOP_SIGN_DETECTED)
+        elif self.current_state == STOP_CAR:
             if self.timeout_stop and (time.time() - self.timeout_stop) > 3:
-                self.change_state("TIMEOUT_STOP") 
-        elif self.current_state == 'parking_state':
+                self.change_state(TIMEOUT_STOP) 
+        elif self.current_state == PARKING_STATE:
             if self.parking_end_time == -1:
-                self.change_state("PARKING_COMPLETED")
+                self.change_state(PARKING_COMPLETED)
             else:
-                self.change_state("TRY_PARKING")
-        elif self.current_state == 'classifying_obstacle':
+                self.change_state(TRY_PARKING)
+        elif self.current_state == CLASSIFYING_OBSTACLE:
             for object_name, valid_distance in objects_detected:
-                if object_name == "PEDESTRIAN" and valid_distance:
-                    self.change_state("OBSTACLE=PEDESTRIAN")
-                elif object_name == "CAR" and valid_distance:
-                    self.change_state("OBSTACLE=CAR")
-                elif object_name == "ROADBLOCK" and valid_distance:
-                    self.change_state("OBSTACLE=ROADBLOCK")
+                if object_name == PEDESTRIAN and valid_distance:
+                    self.change_state(OBSTACLE_PEDESTRIAN)
+                elif object_name == CAR and valid_distance:
+                    self.change_state(OBSTACLE_CAR)
+                elif object_name == ROADBLOCK and valid_distance:
+                    self.change_state(OBSTACLE_ROADBLOCK)
         return self.current_speed, self.current_steer
-     
+    
     def on_roadmap_loaded(self): pass
     def on_obstacle_detected(self): pass
     def on_end(self): pass
@@ -245,7 +237,7 @@ class StateMachine():
     def on_end_of_local_path(self): pass
     def on_timeout_stopline(self): pass
     def on_semaphore_green(self): pass
-
+'''
     #===================== STATE MACHINE MANAGEMENT =====================#
     def run(self):
         """Método principal que se ejecuta en cada iteración del ciclo."""
@@ -273,3 +265,4 @@ class StateMachine():
         print(f'CURR_SIGN: {self.curr_sign}')
         print(f'CURR_LINES: {self.act_lines}')
         print(f'CURR_DEV: {self.act_deviation}')
+'''
